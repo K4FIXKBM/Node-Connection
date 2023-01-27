d638f7a417786e09d9aa8f2fd3309673f639a483
K4FIXKBM <kafisarder@gmail.com>
2023-01-27 15:29:43 +0600

    Node Connection

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http')
+
+http.createServer(function(req , res){
+
+     console.log('My Node JS Server started successfully 1999')
+
+}).listen(5000)                                                            
